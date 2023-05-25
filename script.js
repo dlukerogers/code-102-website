af4ab47e34f5fedf0c36ffeea342287de1f6247c
Daniel Luke Rogers <dluke.rogers@gmail.com>
2023-05-24 19:09:15 -0500

    updated the HTML and CSS. Added some JS

---
+++ script.js
@@ -0,0 +1,43 @@
+let myContainer = 'Hello';
+let myOtherContainer = 'class';
+
+let myAge = 25;
+console.log(myAge);
+myAge = 26;
+
+console.log('Hello from line 7');
+
+const myName = "Luke";
+
+
+
+
+
+alert ("Welcome!");
+const userName = prompt ("What is your name?");
+console.log(userName);
+
+if (userName == "Luke"){
+    document.write("All hail my creator!")
+} else if (userName == "Michelle") {
+    document.write("Hi Mom!")
+}
+else {
+    document.write("Hello amazing person!")
+}
+
+
+document.write(" Welcome to the best website you've ever seen!");
+
+// let result = confirm ("Do you like the color blue?");
+// let message = result ? "Cool" : "Too bad";
+
+// alert(message);
+
+let text;
+if (confirm ("Are you ok with the color blue?") == true) {
+    text = "Cool";
+} else {
+    text = "Too bad"
+}
+alert(text)
